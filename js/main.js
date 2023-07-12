fb4f9b976e276236a97ce25b5f4245f60d0cda0d
Lhyebin <133613625+Lhyebin@users.noreply.github.com>
2023-07-12 09:48:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+    // 콘텐츠 : 슬라이드
+    $('.content_img').slick({
+        arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        // speed: 2000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        fade: true,
+    });
+
+    // 콘텐츠 : 화살표
+    $('.main_content .content_arrows .left').on('click', function () {
+        $('.content_img').slick('slickPrev')
+    })
+    $('.main_content .content_arrows .right').on('click', function () {
+        $('.content_img').slick('slickNext')
+    })
+
+
+
+
+
+
+
+
+    // 인포메이션 : 슬라이드
+    $('.imformation_img').slick({
+        slidesToShow: 4,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+    });
+
+    // 인포메이션 : 화살표
+    $('.main_imformation .imformation_arrows .left').on('click', function () {
+        $('.imformation_img').slick('slickPrev')
+    })
+    $('.main_imformation .imformation_arrows .right').on('click', function () {
+        $('.imformation_img').slick('slickNext')
+    })
+
+    // 인포메이션 : 스테이타임
+    $('.imformation_wrap').on('init afterChange', function (e, s, c) {
+        $('.stay_bar span').animate({ width: ((100 / 5) * c) + 20 + "%" }, 400)
+    })
+
+});
